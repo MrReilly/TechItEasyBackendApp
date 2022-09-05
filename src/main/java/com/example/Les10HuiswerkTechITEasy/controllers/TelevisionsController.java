3a753f7c93577066775440c572b2b5ef5e481c5e
mrReilly <thomas@reilly.nl>
2022-09-05 23:04:30 +0200

    commit2Les11

---
+++ src/main/java/com/example/Les10HuiswerkTechITEasy/controllers/TelevisionsController.java
@@ -0,0 +1,54 @@
+package com.example.Les10HuiswerkTechITEasy.controllers;
+import com.example.Les10HuiswerkTechITEasy.models.Television;
+import com.example.Les10HuiswerkTechITEasy.exceptions.RecordNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class TelevisionsController {
+
+    ArrayList<Television> allTelevisions = new ArrayList<>();
+
+    @GetMapping("/allTelevisions")
+    public ResponseEntity<Object> getTvs() {
+        return ResponseEntity.ok(this.allTelevisions);
+    }
+
+    @GetMapping("/television")
+    public ResponseEntity<Television> getTv(@RequestBody Television t) {
+        return ResponseEntity.ok(t);
+    }
+
+    @PostMapping("/television")
+    public ResponseEntity<Television> createTv(@RequestBody Television t) {
+
+        this.allTelevisions.add(t);
+
+        return ResponseEntity.created();
+    }
+
+    @PutMapping("/television/{id}")
+    public ResponseEntity<Object> updateTv (@PathVariable int id, @RequestBody Television t){
+        if(id < 0 || id >= allTelevisions.size()){
+            throw new RecordNotFoundException("ID cannot be found");
+        }
+
+            this.allTelevisions.set(id, t);
+            return ResponseEntity.noContent();
+        }
+
+    @DeleteMapping("/television/{id}")
+    public ResponseEntity<Object> deleteTv (@PathVariable int id){
+        if(id < 0 || id >= allTelevisions.size()){
+            throw new RecordNotFoundException("ID cannot be found");
+        }
+
+            this.allTelevisions.remove(id);
+            return ResponseEntity.noContent();
+        }
+
+    }
+
+
